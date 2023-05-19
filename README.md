4d8dc3e5e8041b8001788ad9c87578031b7cb965
yw-nam <yw.nam@catenoid.net>
2023-05-19 16:44:17 +0900

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# [Trello] 특정 라벨의 목록별 카드 개수 세기 프로그램
+
+## 필요한 환경변수
+
+- TOKEN : trello api token
+- API_KEY : trello api key
+- BOARD_ID : 해당 trello board의 ID
+
+## 사용법
+
+    TOKEN={트랠로 토큰 값} \
+    API_KEY={트랠로 API 키 값} \
+    BOARD_ID={트랠로 보드 ID} \
+    LABEL={보려는 라벨} \
+    go run main.go
--- main.go
+++ main.go
@@ -1,7 +1,105 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type list struct {
+	Id     string `json:"id"`
+	Name   string `json:"name"`
+	Closed bool   `json:"closed"`
+}
+
+type card struct {
+	Id     string  `json:"id"`
+	Name   string  `json:"name"`
+	Labels []label `json:"labels"`
+}
+
+func (c *card) hasLabel(targetLabel string) bool {
+	for _, label := range c.Labels {
+		if label.Name == targetLabel {
+			return true
+		}
+	}
+	return false
+}
+
+type label struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func mustGetEnv(key string) string {
+	res := os.Getenv(key)
+	if len(res) == 0 {
+		log.Fatalf("%s env is required", key)
+	}
+	return res
+}
 
 func main() {
-	fmt.Println("hello")
+	token := mustGetEnv("TOKEN")
+	apiKey := mustGetEnv("API_KEY")
+	boardId := mustGetEnv("BOARD_ID")
+	targetLabel := mustGetEnv("LABEL")
+
+	lists := []list{}
+	urlGetLists := fmt.Sprintf("https://api.trello.com/1/boards/%s/lists/open?key=%s&token=%s", boardId, apiKey, token)
+	if jsonData, err := getRespJson(urlGetLists); err != nil {
+		log.Fatal(err)
+	} else {
+		if err := json.Unmarshal(jsonData, &lists); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	for _, list := range lists {
+		cards := []card{}
+		urlGetCards := fmt.Sprintf("https://api.trello.com/1/lists/%s/cards?key=%s&token=%s&fields=name,labels", list.Id, apiKey, token)
+		jsonData, err := getRespJson(urlGetCards)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := json.Unmarshal(jsonData, &cards); err != nil {
+			log.Fatal(err)
+		}
+
+		count := countLabel(targetLabel, cards)
+		fmt.Printf("%s: %d\n", list.Name, count)
+	}
+
+}
+
+func getRespJson(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("fail to get request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("response is not OK: %v", resp.Status)
+	}
+
+	jsonData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("fail to read response body: %w", err)
+	}
+	return jsonData, nil
+}
+
+func countLabel(label string, cards []card) int {
+	count := 0
+	for _, c := range cards {
+		if c.hasLabel(label) {
+			count += 1
+		}
+	}
+	return count
 }
